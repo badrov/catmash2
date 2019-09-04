9490d23fc2633b48a23de9abb254588ea55acd83
Badr <badr.nasser@gmail.com>
2019-09-04 07:02:52 +0200

    delete v-gallery

--- catmash-front/src/main.ts
+++ catmash-front/src/main.ts
@@ -2,13 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import vGallery from 'v-gallery';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  vGallery,
   render: (h) => h(App),
 }).$mount('#app');
